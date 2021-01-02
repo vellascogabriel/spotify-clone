2770d71dce0198c3a39af2826c2d8b33a8fea72b
Gabriel Vellasco de Mattos <gabrielvellasco@id.uff.br>
2021-01-01 23:22:25 -0300

    progess bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+
+
 import Sidebar from './components/Sidebar';
+import Player from  './components/Player';
+
+import { Wrapper, Container } from './styles/components';
 
 function App() {
   return (
     <>
     <GlobalStyle/>
-    <Sidebar>
-    </Sidebar>
+    <Wrapper>
+      <Container>
+        <Sidebar/>
+      </Container>
+      <Player/> 
+    </Wrapper>
     </>
   );
 }
